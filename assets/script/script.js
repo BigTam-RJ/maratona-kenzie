73bd4a207bbd122c74b9aa7512652f7f9d40ebfb
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-06-12 11:22:54 -0300

    initial JS code

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,11 @@
+const cart = [];
 
+/* const listProducts = document.getElementsByClassName("product-list");
+console.log(listProducts[0]); */
+const listProducts = document.querySelector(".product-list");
+
+listProducts.addEventListener("click",addToCart);
+
+function addToCart(event){ /* Captura o elemento clickado */
+    console.log(event.target);
+}
