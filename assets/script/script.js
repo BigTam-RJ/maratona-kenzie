c652e63f5c71a3a91fb2da29da0aa16d00870221
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-06-12 13:08:15 -0300

    create functions (render & create products)

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,16 +5,16 @@ console.log(listProducts[0]);
 const listProducts = document.querySelector(".product-list");
 listProducts.addEventListener("click",captureID);*/
 
-function captureTag1(event){ /* Captura a TAG completa do elemento clickado */
+function captureTag1(event) { /* Captura a TAG completa do elemento clickado */
     console.log(event.target);
 }
 
-function captureTag2(event){ /* Captura somente o tipo do elemento clickado */
+function captureTag2(event) { /* Captura somente o tipo do elemento clickado */
     console.log(event.target.tagName);
 }
 
-function captureID(event){ /* Captura somente o id do elemento clickado */
-    if(event.target.tagName==="IMG"){
+function captureID(event) { /* Captura somente o id do elemento clickado */
+    if (event.target.tagName === "IMG") {
         console.log(event.target.id);
     }
 }
@@ -22,24 +22,51 @@ function captureID(event){ /* Captura somente o id do elemento clickado */
 /* ---------- Aqui começa o código --------------  */
 const cart = [];
 const listProducts = document.querySelector(".product-list");
-listProducts.addEventListener("click",addToCart);
+listProducts.addEventListener("click", addToCart);
 
-function addToCart(event){ /* Captura somente o id do elemento clickado */
-    if(event.target.tagName==="IMG"){
+function addToCart(event) { /* Captura somente o id do elemento clickado */
+    if (event.target.tagName === "IMG") {
         const id = event.target.id;
-        for (let i=0; i < products.length; i++) {
-            if (products[i].id === id ) {
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id === id) {
                 cart.push(products[i]);
             }
         }
-        console.log(cart)
+        /* console.log(cart) */
+    }
+    renderCart();
+}
+
+function renderCart() {
+    for (let i = 0; i < cart.length; i++) {
+        createProduct(cart[i]);
     }
 }
 
-const cartProducts = document.querySelector(".products-selected");
-cartProducts.addEventListener("click",renderCart);
+function createProduct(product) {
+    /* captura a ul pela classe */
+    const UL_cart = document.querySelector(".products-selected");
 
-function renderCart(event){
-    
-    console.log(event.target);
+     /* cria uma nova li */
+    const LI_cart = document.createElement("li");
+
+    /* adiciona uma classe na li, caso necessário */
+    /* productCart.className = "classe" */
+
+    /* preenche os elementos na nova li */
+    LI_cart.innerHTML = ` 
+    <img src="${product.img}" alt="${product.title}">
+    <div>
+        <div id="product-description">
+            <h3>"${product.title}"</h3>
+            <button>
+                <img src="./assets/img/trash.png" alt="lixeira">
+            </button>
+        </div>
+        <span>R$"${product.price}"</span>
+    </div>
+    `;
+
+    /* pega a li preenchida e renderiza novamente na ul capturada*/
+    UL_cart.appendChild(LI_cart);
 }
